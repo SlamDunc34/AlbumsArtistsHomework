01c0f0f0f53492b168d9ed1393bfd77ac47c8eaa
Duncan Henderson <duncanroberthenderson@gmail.com>
2017-03-22 16:58:03 +0000

    all lists completed

--- db/console.rb
+++ db/console.rb
@@ -5,14 +5,28 @@ require_relative('../models/albums')
 Album.delete_all
 Artist.delete_all
 
-artist1 = Artist.new({'name' => 'Bad_Brains'})
+artist1 = Artist.new({'name' => 'Bad_Brains', 'genre' => 'Hardcore'})
+artist2 = Artist.new({'name' => 'Kyuss', 'genre' => 'Desert_Punk'})
+
 artist1.save()
+artist2.save()
 
 album1 = Album.new({
   'name' => 'Banned_in_DC',
   'artist_id' => artist1.id
 })
+album2 = Album.new({
+  'name' => 'Wretch',
+  'artist_id' => artist2.id
+})
+album3 = Album.new({
+  'name' => 'Sky_Valley',
+  'artist_id' => artist2.id
+})
+
 album1.save()
+album2.save()
+album3.save()
 
 
 
--- db/music_collection.sql
+++ db/music_collection.sql
@@ -3,7 +3,8 @@ DROP TABLE IF EXISTS artists;
 
 CREATE TABLE artists(
   id SERIAL4 PRIMARY KEY,
-  name VARCHAR(255)
+  name VARCHAR(255),
+  genre VARCHAR(255)
 );
 
 CREATE TABLE albums (
